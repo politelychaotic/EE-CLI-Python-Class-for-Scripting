e3df0b0eaa9a6431a24560b3d3a96b1e68e5a6d2
bash.girlie <94557891+politelychaotic@users.noreply.github.com>
2025-10-28 16:04:51 -0500

    Add files via upload
    Added IndexError checking for camera settings list
    Added check_if_logged_in() to run for every function call to ensure that if token expires during runtime, the script just logs back in and continues

--- py_ee_cli.py
+++ py_ee_cli.py
@@ -5,15 +5,21 @@ from typing import Optional
 import tempfile
 from pathlib import Path
 import csv
+import re
+import contextlib
 
 
 CLI = "./een"
 
+
 CHECK = ["0", "-", "no"]
+
 
 CHECK_CLOUD_RET = ["1"]
 
+AFFIRM = ["yes"]
 
+PATTERN = r"^[1-9]\d*$"
 
 
 class RunCLI:
@@ -38,56 +44,48 @@ class RunCLI:
     :update_cams_by_esn: camfile, setting
 
     """
-    def __init__(self, account_id: str):
+    def __init__(self, account_id: str, username: str, password: str):
         self.account_id = account_id
         self.current_account = account_id
         self.index = 0
         self.account_list = []
+        self.username = username
+        self.password = password
 
 
         # Creating directory for output
         self.working_directory = Path.cwd()
-        self.out_dir = self.working_directory / "output"
+        self.out_dir = self.working_directory / Path("output" + "_" + self.account_id)
         self.account_dir = self.out_dir
 
         try:
-            #self.out_dir.mkdir(exist_ok=False)
-            self.out_dir = Path(str(self.out_dir) + "_" + self.account_id)
             self.out_dir.mkdir(exist_ok=True)
         except Exception as e:
             print(f"An error occurred creating the output directory: {e}")
 
-
-    def check_if_logged_in(self) -> bool:
-        logged_in = False
-        result = self.run([CLI, "account", "list"])
-
-        if result == "error: token expired please login again to continue":
-            logged_in = True
-        elif result == "error: you are already logged in to the account":
-            logged_in = False
-
-        return logged_in
+        self.check_if_logged_in("[+] Logging in...")
 
 
-    
-    def login(self, username: str, password: str):
+    def check_if_logged_in(self, msg: Optional[str] = None):
+        logged_in = subprocess.run([CLI, "account", "list"], capture_output=True, text=True)
+        if logged_in.stderr.strip() == "error: please login to continue":
+            if msg:
+                print(msg)
+            else:
+                print("[->] logging back in...")
+            
+            self.login()
+
+
+    def login(self):
         """
         Log in to either a reseller or subaccount
         """
+        try:
+            self.run([CLI, "auth", "login", "--username", self.username, "--password", self.password])
 
-        if True:
-            print(f"[+] Authenticating: {CLI} auth login --username {username} --password {password}")
-
-            try:
-                subprocess.run([CLI, "auth", "login", "--username", username, "--password", password])
-
-            except Exception as e:
-                print(f"[!!] An error occurred: {e}")
-        
-        if False:
-            print(f"[!!] Already logged in as {self.current_account}")
-
+        except Exception as e:
+            print(f"[!!] An error occurred: {e}")
 
     
     def get_bridges(self):
@@ -101,6 +99,8 @@ class RunCLI:
     
     def get_all_cams(self):
         cam_on_acct = "total_cams.txt"
+
+        self.check_if_logged_in()
 
         # Getting list of cameras on an account
         filepath = str(self.account_dir)+"/"+cam_on_acct
@@ -153,7 +153,7 @@ class RunCLI:
 
         elif not filename:
             """Run CLI command and return stdout."""
-            print(f"Running: {''.join(cmd)}")
+            #print(f"Running: {' '.join(cmd)}")
             result = subprocess.run(cmd, capture_output=True, text=True)
             if result.returncode != 0:
                 print(f"Command failed: {result.stderr}")
@@ -164,11 +164,13 @@ class RunCLI:
     def get_accounts(self):
         account_file = "accounts.txt"
 
+        self.check_if_logged_in()
+
         accounts = subprocess.run([CLI, "account", "list", "-s", "--include", "id"], capture_output=True, text=True)
-        print(accounts.stdout)
+        #print(accounts.stdout)
 
         self.update_account_list(accounts.stdout)    # update account list in instance automatically when you get accounts
-        print("accounts", self.account_list)
+        #print("accounts", self.account_list)
 
         if self.account_list:
             filepath = str(self.out_dir)+"/"+account_file
@@ -191,6 +193,9 @@ class RunCLI:
         Takes no Arguments
         :return:  None
         """
+
+        self.check_if_logged_in()
+
         new_account = self.account_list[self.index]
         print(f"[->] Switching from {self.current_account} -> {new_account}...")
         
@@ -208,11 +213,15 @@ class RunCLI:
 
     
     def switch_account(self, new_account: str) -> None:
+
+        self.check_if_logged_in()
+        
         print(f"[->] Switching from {self.account_id} -> {new_account}...")
         
         try:
             subprocess.run([CLI, "account", "switch", "-s", new_account], capture_output=True, text=True)
             self.current_account = new_account
+            self.account_dir = new_account
 
         
         except Exception as e:
@@ -233,6 +242,8 @@ class RunCLI:
 
         Returns a list of passed cameras
         """
+
+        self.check_if_logged_in()
         
         passed = self.account_dir / "passed.txt"
         failed = self.account_dir / "failed.txt"
@@ -250,24 +261,29 @@ class RunCLI:
                 print(f"[+] Running: {CLI} camera set {setting} {option} --esn {camera}")
                 subprocess.run([CLI, "camera", "set", setting, option, "--esn", camera], capture_output=True, text=True)
                 status = subprocess.run([CLI, "camera", "get", setting, "--esn", camera], capture_output=True, text=True)
-                if status.stdout.split()[-1] in CHECK:
-                    failed_list.append(status.stdout)
-                elif status.stdout.split()[-1] == option:
-                    pass_list.append(status.stdout)
-                else:
-                    print(f"[!!] Error with {camera} : {status.stdout.split()[-1]}")
+                try:
+                    if status.stdout.split()[-1] in CHECK:
+                        failed_list.append(status.stdout)
+                    elif status.stdout.split()[-1] in AFFIRM or re.match(PATTERN, status.stdout.split()[-1]):
+                        pass_list.append(status.stdout)
+                    else:
+                        print(f"[!!] Error with {camera} : {status.stdout.split()[-1]}")
+
+                except IndexError as e:
+                    print('Index Error:{e}')
+
         file.close()
 
         if pass_list:
-            for i in pass_list:
-                with open(passed, "w", newline="\n", encoding="utf-8") as pwrite:
-                    pass
+            for cam in pass_list:
+                with open(passed, "w", encoding="utf-8") as pwrite:
+                    pwrite.write(cam+"\n")
                 pwrite.close()
 
         if failed_list:
-            for i in failed_list:
-                with open(failed, "w", newline="\n", encoding="utf-8") as fwrite:
-                    pass
+            for cam in failed_list:
+                with open(failed, "w", encoding="utf-8") as fwrite:
+                    fwrite.write(cam+"\n")
                 fwrite.close()
 
         return pass_list
@@ -296,39 +312,48 @@ class RunCLI:
     def get_all_camera_settings_by_esn(self, file: str, setting: str, ret: int, save: Optional[bool]=True) -> list | tuple[list,list] | None:
         """
         :save: chose to save files or not, set to False to overide save
-        :ret: returns a list: 0 -> not_set_cams, 1 -> set_cams, 2 -> not_set_cams, set_cams
+        :ret: returns a list: 0 -> not_set_cams, 1 -> set_cams, 2 -> unknown_file, 3 -> not_set_cams, set_cams
         """
 
         not_set_cams = []
         set_cams = []
+        unknown = []
 
-        not_set_file = self.account_dir / "not_set_cams.txt"
-        set_file = self.account_dir / "set_cams.txt"
+        not_set_file = self.account_dir / Path("not_set_cams.txt")
+        set_file = self.account_dir / Path("set_cams.txt")
+        unknown_file = self.account_dir / Path("unknown_settings.txt")
         not_set_file = str(not_set_file)
         set_file = str(set_file)
+        unknown_file = str(unknown_file)
 
+        
+        self.check_if_logged_in()
 
     
         with open(file, "r", encoding="utf-8") as reader:
             cameras = reader.readlines()
             for camera in cameras:
                 camera = camera.strip()
-                print(f"[+] Getting camera settings")
+                print(f"[+] Getting camera settings {camera}")
                 setting_value = subprocess.run([CLI, "camera", "get", setting, "--esn", camera], capture_output=True, text=True)
-                setting_value = setting_value.stdout.split()[-1]    # Get last column and interpret as setting value
-                if setting == "cloud-retention":
-                    check = CHECK_CLOUD_RET
-                if setting_value in check:
-                    not_set_cams.append(camera)
-                elif setting_value not in check:
-                    set_cams.append(camera)
-                else:
-                    print("[!!] Issue figuring out value")
+                try:
+                    setting_value = setting_value.stdout.split()[-1]    # Get last column and interpret as setting value
+                    if setting == "cloud-retention":
+                        check = CHECK_CLOUD_RET
+                    if setting_value in check:
+                        not_set_cams.append(camera)
+                    elif setting_value not in check:
+                        set_cams.append(camera)
+                except IndexError as e:
+                    print(f'[!!] Index error {e} for {camera}: {setting_value}')
+                    print(f'[=>] Saving {camera} -> Unknown settings list: {unknown_file}')
+                    unknown.append(camera)
         
         reader.close()
 
-        print(set_cams)
-        print(not_set_cams)
+        print(f"[+] Cameras NOT found in CHECK: {set_cams}")
+        print(f"[+] Cameras found in CHECK: {not_set_cams}")
+        print(f"[+] Cameras with UNKNOWN settings: {unknown}")
 
 
         if save:
@@ -341,6 +366,11 @@ class RunCLI:
                 for cam in not_set_cams:
                     no_value.write(cam+"\n")
             no_value.close()
+
+            with open(unknown_file, "w", encoding='utf-8') as unknown_value:
+                for cam in unknown:
+                    unknown_value.write(cam+"\n")
+            unknown_value.close()
         
 
         # return list based on what we want returned for our use case
@@ -349,7 +379,10 @@ class RunCLI:
         elif ret == 1:
             return set_cams
         elif ret == 2:
-            return not_set_cams, set_cams
+            return unknown_file
+        elif ret == 3:
+            return unknown_file, not_set_cams, set_cams
+        
         return None
     
                 
